d0673eb7220ecbb2e9485d9df95203016c5c73ce
Roberto Serrano Guerrero <Roberto Serrano Guerrero@LAPTOP-TEB7U51M>
2017-12-28 20:26:08 +0100

    commit 4

--- src/main/java/es/juntadeandalucia/chap/osTest/OsTestApplication.java
+++ src/main/java/es/juntadeandalucia/chap/osTest/OsTestApplication.java
@@ -2,16 +2,14 @@ package es.juntadeandalucia.chap.osTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OsTestApplication  extends SpringBootServletInitializer{
+public class OsTestApplication  {
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(OsTestApplication.class);
-	}
+//	@Override extends SpringBootServletInitializer
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(OsTestApplication.class);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsTestApplication.class, args);
