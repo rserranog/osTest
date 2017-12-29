4f0101949ab46c449a5633cd6e919dd13266df87
Roberto Serrano Guerrero <rserrano@emergya.com>
2017-12-29 13:43:25 +0100

    Mapeos de Error y Raiz

--- src/main/java/es/juntadeandalucia/chap/osTest/MainController.java
+++ src/main/java/es/juntadeandalucia/chap/osTest/MainController.java
@@ -1,19 +1,49 @@
 package es.juntadeandalucia.chap.osTest;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Map.Entry;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @RestController
 public class MainController {
 
+	@Autowired
+	private RequestMappingHandlerMapping handlerMapping;
+	
 //	@Value("${nombre.nodo}")
 	String nombreNodo = "Nodo 1";
 	
-	@GetMapping("/hola")
+	@RequestMapping("/hola")
 	public Mensaje holaMundo(){
 		return new Mensaje("hola, soy " + nombreNodo);
+	}
+	
+	@RequestMapping("/error")
+	public Mensaje error(){
+		return new Mensaje("Ha ocurrido un error");
+	}
+	
+	@RequestMapping("/")
+	public ArrayList<String> base(){
+		ArrayList<String> mapeos = new ArrayList<String>();
+		
+		for(Entry<RequestMappingInfo, HandlerMethod> mapeo : handlerMapping.getHandlerMethods().entrySet()){
+			String mapeoSt = mapeo.getKey().getPatternsCondition().toString();
+			if(!mapeos.contains(mapeoSt)){
+				mapeos.add(mapeoSt);
+			}
+		}
+		
+		return mapeos;
 	}
 }
 
