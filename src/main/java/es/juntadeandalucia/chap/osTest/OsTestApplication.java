f1438fe6f7692194f02e69dc460761cdf699cdc2
Roberto Serrano Guerrero <rserrano@emergya.com>
2017-12-29 14:26:53 +0100

    Test

--- src/main/java/es/juntadeandalucia/chap/osTest/OsTestApplication.java
+++ src/main/java/es/juntadeandalucia/chap/osTest/OsTestApplication.java
@@ -5,13 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OsTestApplication  {
-	
-//	@Override extends SpringBootServletInitializer
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(OsTestApplication.class);
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsTestApplication.class, args);
 	}
+	
 }
